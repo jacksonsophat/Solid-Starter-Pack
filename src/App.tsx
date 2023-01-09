c83fdb6e491e82ccb9da615e9af6e04fc5291850
jacksonsophat <37005847+jacksonsophat@users.noreply.github.com>
2023-01-09 16:43:30 -0600

    I guess this is second commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import type { Component } from 'solid-js';
+
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+const App: Component = () => {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline bg-purple-50 py-5">
+        This is the setup for Solid with styles:
+      </h1>
+      <div class="my-4">
+        <p class='my-2'>Tailwind css</p>
+        {/* <p class='my-2'>Tailwind css</p> */}
+      </div>
+    </div>
+  );
+};
+
+export default App;
